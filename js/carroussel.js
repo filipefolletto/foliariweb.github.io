b2d609625f2fada86a0ca0d54bf44e8fba641027
ViniZanetti <vinizanetti14@gmail.com>
2023-10-09 02:21:59 -0300

    estrutura carrocel pronta,, substituir barra de rolagem horizontal pelas flexinha

--- js/carroussel.js
+++ js/carroussel.js
@@ -7,12 +7,8 @@ const fotos = [
 'cadeiras',
 'dreamspet',
 'poltronas',
-'vestuario',]
-
-
-// fotos.forEach(card => {
-//     card = createElement()
-// });
+'vestuario',
+]
 
 
 
@@ -22,18 +18,18 @@ const createElement = (tag, className) => {
     return element
 }
 
-const creatCard = (personagem) => {
+const creatCard = (imgs) => {
     const card = createElement('div', 'card-produto')
 
     const divCardImg = createElement('div', 'div-card-img')
     const imgProduto = createElement('img', 'imgProduto')
     // imgProduto.src = `url(../img/${fotos}.png)`
-    imgProduto.src = `../img/${fotos[1]}.png`
+    imgProduto.src = `../img/${imgs}.png`
   
 
     const divNomeProduto = createElement('div', 'div-nome-produto')
     const h4Nome = createElement('h4', 'nomeProduto')
-    h4Nome.innerHTML = 'ahhahahah'
+    h4Nome.innerHTML = `${imgs}`
 
     
     
@@ -45,54 +41,24 @@ const creatCard = (personagem) => {
     card.appendChild(divCardImg)
     card.appendChild(divNomeProduto)
 
-    paiCard.appendChild(card)
     
     // console.log(paiCard)
-    return paiCard
+    return card
 
-    // paiCard .appendChild(card)
     // card.setAttribute('data-personagem', personagem)
     
 }
 
-creatCard()
+fotos.forEach((imgs) => {
+
+    const cardImgs = creatCard(imgs)
+    paiCard.appendChild(cardImgs)
+});
 
 
 
+window.onload = () => { 
 
+    creatCard()    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const createElement = (tag, className) => {
-//     const element = document.createElement(tag)
-//     element.className = className;
-//     return element
-// }
-
-
-
-//     front.style.backgroundImage = `url('../images/${personagem}.png')`
-//     // card.className = "card"
-//    // front.className = "face front"
-//    // back.className = "face back"
-
-
-//     card.appendChild(front)
-//     card.appendChild(back)
-//     // grid.appendChild(card)
-
-//     card.addEventListener('click', revealCard)
-//     card.setAttribute('data-personagem', personagem)
-
-//     return card
-// }
