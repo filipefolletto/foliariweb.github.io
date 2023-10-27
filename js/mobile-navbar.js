f968fa4a66b9b00a9e3f0faf216e33d09aa30b7d
ViniZanetti <vinizanetti14@gmail.com>
2023-10-27 15:00:39 -0300

    27/10 as 15:00

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,16 +1,17 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, /*navLinks*/) {
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks)
         this.activeClass = "active"
         this.handleClick = this.handleClick.bind(this);
     }
 
-    // animateLinks() {
-    //     this.navLinks.forEach((link, index) => {
-    //       link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s `);
-    //     });
-    // }
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s `);
+        });
+    }
 
     
     handleClick() {
